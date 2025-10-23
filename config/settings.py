69abf7f5b34fc0205913446be7c61b573867617d
Fabricio Barrientos Takata <fbarrientostakata@gmail.com>
2025-10-23 01:19:49 -0400

    deps

--- config/settings.py
+++ config/settings.py
@@ -50,10 +50,10 @@ AUTH_USER_MODEL = 'usuarios.Usuario'
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
